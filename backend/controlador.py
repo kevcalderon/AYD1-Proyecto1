aee0335fece78616db8c477d85ef3881615d80e3
DouglasSochC <3008474750101@ingenieria.usac.edu.gt>
2023-04-14 11:28:45 -0600

    [FIX] Se ha corregido el retorno de informacion del endpoint en el caso que encuentre un usuario, además se ha validado que el usuario este activo para que este pueda loguearse al sistema.

--- backend/controlador.py
+++ backend/controlador.py
@@ -37,11 +37,29 @@ def LoguearEmpresa(nombre_empresa, contrasenia):
     conexion = obtener_conexion()
     empresa = None
     with conexion.cursor() as cursor:
-        cursor.execute(
-            "SELECT EMP_ID, CONTRASENA FROM EMPRESA e WHERE USUARIO = %s", (nombre_empresa,))
+        cursor.execute("""SELECT e.CONTRASENA, e.EMP_ID, e.NOMBRE AS NOMBRE_EMPRESA, e.DESCRIPCION, e.CORREO, e.TELEFONO, e.USUARIO, e.NIT, e.ESTADO, e.DOCUMENTO, 
+            te.T_EMP_ID, te.NOMBRE AS NOMBRE_TIPO_EMPRESA, d.DIR_ID, d.LUGAR 
+            FROM EMPRESA e
+            INNER JOIN TIPO_EMPRESA te ON te.T_EMP_ID = e.TIPO_EMPRESA_T_EMP_ID 
+            INNER JOIN DIRECCION d ON d.DIR_ID = e.DIRECCION_DIR_ID  WHERE USUARIO = %s AND ESTADO = 'activo'""", (nombre_empresa,))
         empresa = cursor.fetchone()
-        if empresa and check_password_hash(empresa[1], contrasenia):
-            empresa = empresa[0]
+        if empresa and check_password_hash(empresa[0], contrasenia):
+            empresa = {
+                # 'CONTRASENA': empresa[0],
+                'EMP_ID': empresa[1],
+                'NOMBRE_EMPRESA': empresa[2],
+                'DESCRIPCION': empresa[3],
+                'CORREO': empresa[4],
+                'TELEFONO': empresa[5],
+                'USUARIO': empresa[6],                
+                'NIT': empresa[7],
+                # 'ESTADO': empresa[8],
+                'DOCUMENTO': empresa[9],
+                'T_EMP_ID': empresa[10],
+                'NOMBRE_TIPO_EMPRESA': empresa[11],
+                'DIR_ID': empresa[12],
+                'LUGAR': empresa[13],
+            }
         else:
             empresa = None
     conexion.close()
