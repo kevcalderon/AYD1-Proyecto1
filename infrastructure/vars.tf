2ae90867b4b6deace8bb13d02e46546348627b66
2730538920101 <2730538920101@ingenieria.usac.edu.gt>
2023-05-02 21:54:36 -0600

    [ADD] Dockerfiles, update del compose y se agrego infraestructura con TF

---
+++ infrastructure/vars.tf
@@ -0,0 +1,13 @@
+variable "AWS_ACCESS_KEY" {}
+variable "AWS_SECRET_KEY" {}
+variable "AWS_REGION" {}
+variable "proyecto_ayd1_vpc_cidr" {}
+variable "public1" {}
+variable "private1" {}
+variable "public2" {}
+variable "private2" {}
+variable "user_ssh" {}
+variable "PATH_KEYPAIR" {}
+variable "PATH_PUBLIC_KEYPAIR" {}
+
+
