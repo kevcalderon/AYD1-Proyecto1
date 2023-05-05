caad3df1e2a1642f60c57da7d0906643b241030a
2730538920101 <2730538920101@ingenieria.usac.edu.gt>
2023-05-05 14:36:13 -0600

    [FIX] Ultima hora

--- infrastructure/ec2.tf
+++ infrastructure/ec2.tf
@@ -89,9 +89,7 @@ resource "aws_instance" "ec2_bastion" {
       "sudo curl -L \"https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)\" -o /usr/local/bin/docker-compose",
       "sudo chmod +x /usr/local/bin/docker-compose",
       "sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose",
-      "git clone https://github.com/kevcalderon/AYD1-Proyecto1.git",
-      "cd AYD1-Proyecto1",
-      "docker-compose up -d"
+      "git clone https://github.com/kevcalderon/AYD1-Proyecto1.git"
     ]
   }
   connection {
